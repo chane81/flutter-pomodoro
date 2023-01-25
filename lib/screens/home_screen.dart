087cdaf422ba32bec44caddf5d66c9fa249bdff4
chane81 <chane81@naver.com>
2023-01-26 01:03:53 +0900

    feat: timer pause & play

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = 1500;
+  bool isRunning = false;
   Timer? timer;
 
   void onTick(Timer timer) {
@@ -20,7 +21,19 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void onStartPressed() {
-    timer ??= Timer.periodic(const Duration(seconds: 1), onTick);
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer?.cancel();
+
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -48,8 +61,10 @@ class _HomeScreenState extends State<HomeScreen> {
             child: IconButton(
               iconSize: 120,
               color: Theme.of(context).cardColor,
-              onPressed: onStartPressed,
-              icon: const Icon(Icons.play_circle_outline),
+              onPressed: isRunning ? onPausePressed : onStartPressed,
+              icon: Icon(isRunning
+                  ? Icons.pause_circle_outline
+                  : Icons.play_circle_outline),
             ),
           ),
         ),
